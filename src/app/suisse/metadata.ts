e738b1c2eff34c5f245b5392a7fa9ec80ed71773
Dali-Math <momo18669@gmail.com>
2025-10-05 10:48:53 +0200

    Delete src/app/suisse/metadata.ts

--- src/app/suisse/metadata.ts
+++
@@ -1,7 +0,0 @@
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Horlogerie Suisse | AFP Horlogerie",
-  description:
-    "Découvrez l'histoire, les grandes manufactures, les savoir-faire et les écoles qui ont fait de la Suisse le berceau mondial de l'horlogerie."
-};
