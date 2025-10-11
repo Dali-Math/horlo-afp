d34cf39df782f173548a8f0b51d2aaffdcbfdb21
Dali-Math <momo18669@gmail.com>
2025-10-11 17:39:12 +0200

    fix: use public image paths for MemoryGame assets

--- src/app/quiz/mouvement-6497/MemoryGame.tsx
+++ src/app/quiz/mouvement-6497/MemoryGame.tsx
@@ -1,39 +1,16 @@
 "use client";
-
 import React, { useState, useEffect } from 'react';
-
-// Image imports
-import img1 from './images/1.png';
-import img2 from './images/2.png';
-import img3 from './images/3.png';
-import img4 from './images/4.png';
-import img5 from './images/5.png';
-import img6 from './images/6.png';
-import img7 from './images/7.png';
-import img8 from './images/8.png';
-import img9 from './images/9.png';
-import img10 from './images/10.png';
-import img11 from './images/11.png';
-import img12 from './images/12.png';
-import img13 from './images/13.png';
-import img14 from './images/14.png';
-import img15 from './images/15.png';
-import img16 from './images/16.png';
-import img17 from './images/17.png';
-import img18 from './images/18.png';
 
 interface Card {
   id: number;
-  image: any;
+  image: string;
   isFlipped: boolean;
   isMatched: boolean;
 }
 
 export default function MemoryGame() {
-  const images = [
-    img1, img2, img3, img4, img5, img6, img7, img8, img9,
-    img10, img11, img12, img13, img14, img15, img16, img17, img18
-  ];
+  // Use public folder paths instead of imports
+  const images = Array.from({ length: 18 }, (_, i) => `/images/quiz/6497/${i + 1}.png`);
 
   const [cards, setCards] = useState<Card[]>([]);
   const [flippedCards, setFlippedCards] = useState<number[]>([]);
@@ -273,7 +250,7 @@ export default function MemoryGame() {
               <div className="card-inner">
                 <div className="card-front">?</div>
                 <div className="card-back">
-                  <img src={card.image.src} alt={`Carte ${card.id + 1}`} />
+                  <img src={card.image} alt={`Carte ${card.id + 1}`} />
                 </div>
               </div>
             </div>
