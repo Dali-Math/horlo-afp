549ea268cbefd035d509417c73115af979286389
Dali-Math <momo18669@gmail.com>
2025-10-12 17:52:40 +0200

    fix(config): force single-locale setup (fr only)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  i18n: {
+    locales: ["fr"],
+    defaultLocale: "fr",
+  },
   trailingSlash: false,
+  reactStrictMode: true,
   output: "standalone",
 };
 
