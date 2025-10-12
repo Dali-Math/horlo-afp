c232fccce44fd320bbf6397dfb86c7e77d857f4a
Dali-Math <momo18669@gmail.com>
2025-10-12 20:48:09 +0200

    fix(config): remove i18n to allow GitHub Pages static export

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ["fr"],
-    defaultLocale: "fr",
-  },
   trailingSlash: false,
   reactStrictMode: true,
   output: "standalone",
 };
-
 module.exports = nextConfig;
