4e7bb00017563b74ea9ed569d8794df42a737934
Dali-Math <momo18669@gmail.com>
2025-10-12 16:27:50 +0200

    fix(config): remove i18n config, use next-intl middleware only

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ["fr", "en"],
-    defaultLocale: "fr",
-  },
   trailingSlash: false,
   reactStrictMode: true,
   output: "standalone",
-
-  // Supprimer complètement la section "redirects"
-  // Elle n'est plus nécessaire puisque defaultLocale est déjà "fr".
 };
 
 module.exports = nextConfig;
