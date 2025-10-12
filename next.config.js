376a83e8889f3f378aa67a99e50200ac9bc5fc08
Dali-Math <momo18669@gmail.com>
2025-10-12 16:48:49 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    locales: ["fr", "en"],
+    defaultLocale: "fr",
+  },
   trailingSlash: false,
   reactStrictMode: true,
   output: "standalone",
