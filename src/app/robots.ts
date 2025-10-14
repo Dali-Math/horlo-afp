8e707658d0f7cde8413748770f8a56fcf7aa315f
Dali-Math <momo18669@gmail.com>
2025-10-14 13:15:57 +0200

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,15 +1,12 @@
-import { MetadataRoute } from "next";
 import { SITE } from "@/lib/seo";
 
-export default function robots(): MetadataRoute.Robots {
+export default function robots() {
   return {
     rules: [
       { userAgent: "*", allow: "/" },
-      { userAgent: "GPTBot", allow: "/" },
-      { userAgent: "PerplexityBot", allow: "/" },
       { userAgent: "Google-Extended", allow: "/" },
     ],
-    sitemap: `${SITE.domain}/sitemap.xml`,
-    host: SITE.domain.replace("https://", ""),
+    sitemap: `${SITE.url}/sitemap.xml`,
+    host: SITE.url.replace("https://", ""),
   };
 }
