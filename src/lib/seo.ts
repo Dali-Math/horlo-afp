821a50e98ed8ba087505412f6d11bcc14646ed79
Dali-Math <momo18669@gmail.com>
2025-10-14 18:06:22 +0200

    Update seo.ts

--- src/lib/seo.ts
+++ src/lib/seo.ts
@@ -1,11 +1,18 @@
 export const SITE = {
   name: "HorloLearn",
-  url: "https://www.horlolearn.ch",
-  title: "HorloLearn | Formation AFP en Horlogerie Suisse",
-  description:
-    "Plateforme éducative dédiée à la formation AFP en horlogerie suisse : théorie, pratique, fiches, vidéos et quiz interactifs.",
-  image: "/og-image.svg",
-  twitter: { site: "@HorloLearn", creator: "@HorloLearn" },
-  organization: { legalName: "HorloLearn" },
+  domain: "https://www.horlolearn.ch",
+  locale: "fr_CH",
+  twitter: "@HorloLearn",
+  logo: "/og-image.jpg",
+  contactEmail: "mathlouthi_mohamed82@yahoo.com",
+  organization: {
+    legalName: "HorloLearn",
+    url: "https://www.horlolearn.ch",
+    logo: "https://www.horlolearn.ch/og-image.jpg",
+    sameAs: [
+      "https://github.com/Dali-Math",
+      "https://www.youtube.com/@HorloLearn",
+      "https://www.linkedin.com/in/...",
+    ],
+  },
 };
-// ⛔️ pas de SITE.domain / pas de SITE.logo
