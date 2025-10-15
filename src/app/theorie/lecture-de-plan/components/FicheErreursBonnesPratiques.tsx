2c1a63712b24f08b682de0ef732aa11353735e6b
Dali-Math <momo18669@gmail.com>
2025-10-15 15:57:13 +0200

    Update and rename FicheErreursBonnesPratiques.tsx to FicheTest.tsx

--- src/app/theorie/lecture-de-plan/components/FicheErreursBonnesPratiques.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-export default function FicheErreursBonnesPratiques() {
-  return (
-    <div style={{color:"white",fontWeight:"bold",fontSize:24, background:"red", padding:32, textAlign:"center"}}>
-      TEST FICHE
-    </div>
-  );
-}
