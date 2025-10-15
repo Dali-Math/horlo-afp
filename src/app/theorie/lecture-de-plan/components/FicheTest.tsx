5a30d2e144588d79812d183d885db7db4eedfeb9
Dali-Math <momo18669@gmail.com>
2025-10-15 16:10:25 +0200

    Update FicheTest.tsx

--- src/app/theorie/lecture-de-plan/components/FicheTest.tsx
+++ src/app/theorie/lecture-de-plan/components/FicheTest.tsx
@@ -2,7 +2,15 @@
 import React from "react";
 export default function FicheTest() {
   return (
-    <div style={{background:"red", color:"white", fontWeight:"bold", fontSize:32, textAlign:"center", margin:50, padding:50}}>
+    <div style={{
+      background:"red",
+      color:"white",
+      fontWeight:"bold",
+      fontSize:32,
+      textAlign:"center",
+      margin:50,
+      padding:50
+    }}>
       TEST FICHE — OK STRUCTURE
     </div>
   );
