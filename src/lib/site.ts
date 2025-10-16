39767ffdb027a204cc0e249953a8e07259571cfe
Dali-Math <momo18669@gmail.com>
2025-10-16 21:28:52 +0200

    Create site.ts

---
+++ src/lib/site.ts
@@ -0,0 +1,18 @@
+export const SITE = {
+  name: "HorloLearn",
+  domain: "https://www.horlolearn.ch",
+  locale: "fr_CH",
+  twitter: "@HorloLearn",
+  logo: "/og-image.jpg",
+  contactEmail: "mathlouthi_mohamed82@yahoo.com",
+  organization: {
+    legalName: "HorloLearn",
+    url: "https://www.horlolearn.ch",
+    logo: "https://www.horlolearn.ch/og-image.jpg",
+    sameAs: [
+      "https://github.com/Dali-Math",
+      "https://www.youtube.com/@HorloLearn",
+      "https://www.linkedin.com/in/...",
+    ],
+  },
+};
