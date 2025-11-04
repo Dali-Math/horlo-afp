ea96952036216659aabb8a86c68a24f6f38d23c2
Dali-Math <momo18669@gmail.com>
2025-11-04 20:36:23 +0100

    Update index.ts

--- src/app/theorie/histoire-horlogerie-suisse/components/index.ts
+++ src/app/theorie/histoire-horlogerie-suisse/components/index.ts
@@ -1,7 +1,7 @@
 // components/index.ts
 export { Navigation } from './Navigation';
 export { Hero } from './Hero';
-export { Timeline } from './Timeline';
+export { default as Timeline } from './Timeline';
 export { Regions } from './Regions';
 export { Manufactures } from './Manufactures';
 export { Footer } from './Footer';
