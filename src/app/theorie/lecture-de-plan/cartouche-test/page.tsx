9022cbc311ac0768b6f2efb4765e9e8d1a3256a0
Dali-Math <momo18669@gmail.com>
2025-11-05 11:20:40 +0100

    Create page.tsx

---
+++ src/app/theorie/lecture-de-plan/cartouche-test/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react'
+import CartoucheHorloger from '@/components/cartouche/InteractiveCartouche'
+
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <CartoucheHorloger />
+    </main>
+  )
+}
