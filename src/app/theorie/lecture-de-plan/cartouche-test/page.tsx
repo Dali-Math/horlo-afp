ab78b7f8349a7af788bc2a493f65f674e0b19df3
Dali-Math <momo18669@gmail.com>
2025-11-05 11:43:51 +0100

    Update page.tsx

--- src/app/theorie/lecture-de-plan/cartouche-test/page.tsx
+++ src/app/theorie/lecture-de-plan/cartouche-test/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from 'react'
-import InteractiveCartouche from '@/components/cartouche/InteractiveCartouche'
+import { InteractiveCartouche } from '@/components/cartouche/InteractiveCartouche'
 
 export default function Page() {
   return (
