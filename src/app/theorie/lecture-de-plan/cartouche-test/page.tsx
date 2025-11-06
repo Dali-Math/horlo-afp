0d500544676bca5f4684af3c94a1bf44055a8125
Dali-Math <momo18669@gmail.com>
2025-11-06 09:52:32 +0100

    Update page.tsx

--- src/app/theorie/lecture-de-plan/cartouche-test/page.tsx
+++ src/app/theorie/lecture-de-plan/cartouche-test/page.tsx
@@ -361,4 +361,11 @@ const FAQSection = ({ darkMode }: any) => {
     </div>
   );
 };
+const Page = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-500">Page test du cartouche</h1>
+    </div>
+  );
+};
 export default Page;
