2d1c3b2390e78916147018ffa110092324539249
Dali-Math <momo18669@gmail.com>
2025-11-07 22:19:13 +0100

    Update page.tsx

--- src/app/theorie/rouage/page.tsx
+++ src/app/theorie/rouage/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState, useEffect, useRef } from 'react';
 import { Clock, Zap, Gauge, Settings, ChevronRight, PlayCircle, PauseCircle, RotateCw, Info, ChevronDown } from 'lucide-react';
 
