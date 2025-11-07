16ac8ef97dc0352a45b8def3d83b6f6024facdd9
Dali-Math <momo18669@gmail.com>
2025-11-07 13:09:42 +0100

    Update ThemeProvider.tsx

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,27 +1,16 @@
-'use client'
-import * as React from 'react'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
 
-interface Props {
-  children: React.ReactNode
-  attribute?: string
-  defaultTheme?: string
-  enableSystem?: boolean
-}
+"use client";
+
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import type { ThemeProviderProps } from "next-themes";
 
 export default function ThemeProvider({
   children,
-  attribute = 'class',
-  defaultTheme = 'dark',
-  enableSystem = true,
-}: Props) {
+  ...props
+}: ThemeProviderProps) {
   return (
-    <NextThemesProvider
-      attribute={attribute}
-      defaultTheme={defaultTheme}
-      enableSystem={enableSystem}
-    >
+    <NextThemesProvider attribute="class" defaultTheme="dark" {...props}>
       {children}
     </NextThemesProvider>
-  )
+  );
 }
