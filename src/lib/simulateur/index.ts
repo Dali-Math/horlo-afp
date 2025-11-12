f3e25c3c826b7849c621d760934e9cfa121f3858
Dali-Math <momo18669@gmail.com>
2025-11-12 13:16:17 +0100

    Refactor exports in index.ts for better organization

--- src/lib/simulateur/index.ts
+++ src/lib/simulateur/index.ts
@@ -1,9 +1,6 @@
-// Point d'entrée central pour toutes les fonctions du simulateur
-export * from './types';
-export * from './technical-specs';
-export * from './simulation-engine';
-export * from './diagnostics';
-export * from './export-utils';
-
-// Fonctions de commodité
-export { MOUVEMENTS_DB as default } from './technical-specs';
+export * from './types'
+export * from './specs/technical'
+export * from './simulation-engine'
+export * from './diagnostics'
+export * from './export-utils'
+export { MOUVEMENTS_DB as default } from './specs/technical'
