d136f330396d2976597c61b44083795e0ebd96e8
Dali-Math <momo18669@gmail.com>
2025-11-12 13:02:55 +0100

    Create central entry point for simulator functions
    This file serves as the central entry point for all simulator functions, exporting various modules and providing convenience functions.

---
+++ src/lib/simulateur/index.ts
@@ -0,0 +1,9 @@
+// Point d'entrée central pour toutes les fonctions du simulateur
+export * from './types';
+export * from './technical-specs';
+export * from './simulation-engine';
+export * from './diagnostics';
+export * from './export-utils';
+
+// Fonctions de commodité
+export { MOUVEMENTS_DB as default } from './technical-specs';
