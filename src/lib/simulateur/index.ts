519e459b6eb13860c6f7a21cd1baae91585053b1
Dali-Math <momo18669@gmail.com>
2025-11-12 23:53:29 +0100

    Delete src/lib/simulateur directory

--- src/lib/simulateur/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './database';
-export * from './simulation-engine';
-export * from './diagnostics';
-export * from './export-utils';
-export { TECHNICAL_SPECS as default } from './database';
