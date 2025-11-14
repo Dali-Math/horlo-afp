2bb0d4596a84783b89ff920ccab81139dbd932af
Dali-Math <momo18669@gmail.com>
2025-11-14 17:58:21 +0100

    Add type annotation to mouse move event handler

--- src/app/materiaux/page.tsx
+++ src/app/materiaux/page.tsx
@@ -9,7 +9,7 @@ export default function MateriauxHomePage() {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
-    const handleMouseMove = (e) => {
+    const handleMouseMove = (e: MouseEvent) => {
       setMousePosition({ x: e.clientX, y: e.clientY });
     };
     window.addEventListener('mousemove', handleMouseMove);
