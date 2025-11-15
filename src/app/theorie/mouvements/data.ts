ac4f1d268b846dd0068c60ab6d73278a0802612f
Dali-Math <momo18669@gmail.com>
2025-11-15 16:08:02 +0100

    Rename modules to concepts in data.ts

--- src/app/theorie/mouvements/data.ts
+++ src/app/theorie/mouvements/data.ts
@@ -1,6 +1,6 @@
-import type { Module } from './types'; 
+import type { Concept } from './types';
 
-export const modules: Module[] = [
+export const concepts: Concept[] = [
   {
     id: 'architecture',
     title: "🏗️ Architecture du Mouvement",
