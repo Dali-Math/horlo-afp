75ebb9374cfc01f7c3fd2f28032f68944b71f58b
Dali-Math <momo18669@gmail.com>
2025-11-16 22:53:22 +0100

    Fix import statement for MontreMecaniqueComplete

--- src/app/theorie/introduction-montre-mecanique/page.tsx
+++ src/app/theorie/introduction-montre-mecanique/page.tsx
@@ -1,5 +1,4 @@
-"use client";
-import { MontreMecaniqueComplete } from '@/components/montre/MontreMecaniqueComplete';
+import MontreMecaniqueComplete from '@/components/montre/MontreMecaniqueComplete';
 
 export default function IntroductionMontreMecaniquePage() {
   return <MontreMecaniqueComplete />;
