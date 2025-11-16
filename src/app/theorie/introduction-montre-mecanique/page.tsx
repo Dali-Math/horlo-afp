6fd14ece24066eef2d30ffacfa3f78ce2525e79b
Dali-Math <momo18669@gmail.com>
2025-11-16 23:46:47 +0100

    Update page.tsx

--- src/app/theorie/introduction-montre-mecanique/page.tsx
+++ src/app/theorie/introduction-montre-mecanique/page.tsx
@@ -1,15 +1,10 @@
-import { MontreProvider } from '@/components/montre/MontreMecaniqueProvider';
 import MontreMecaniqueComplete from '@/components/montre/MontreMecaniqueComplete';
+
+export default function IntroductionMontreMecaniquePage() {
+  return <MontreMecaniqueComplete />;
+}
 
 export const metadata = {
   title: 'Introduction aux Montres Mécaniques | Horlolearn',
   description: 'Découvrez le fonctionnement fascinant des montres mécaniques',
 };
-
-export default function IntroductionMontreMecaniquePage() {
-  return (
-    <MontreProvider>
-      <MontreMecaniqueComplete />
-    </MontreProvider>
-  );
-}
