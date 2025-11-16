eef57b745fe55cc6a28782c8cddcaed0e09b1912
Dali-Math <momo18669@gmail.com>
2025-11-16 22:51:17 +0100

    Create layout component for mechanical watch introduction

---
+++ src/app/theorie/introduction-montre-mecanique/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Introduction aux Montres Mécaniques | Horlolearn",
+  description: "Découvrez le fonctionnement fascinant des montres mécaniques",
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return children;
+}
