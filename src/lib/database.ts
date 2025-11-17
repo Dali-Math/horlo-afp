fedc36ebd47a55ed60f59f71c4639a03b121d19e
Dali-Math <momo18669@gmail.com>
2025-11-17 21:37:54 +0100

    Delete src/lib/database.ts

--- src/lib/database.ts
+++
@@ -1,7 +0,0 @@
-// src/lib/database.ts
-import spiralsDatabase from './spirals-database.json';
-
-export const database = spiralsDatabase;
-
-export type MaterialKey = keyof typeof database.materials;
-export type FrequencyKey = keyof typeof database.frequencies;
