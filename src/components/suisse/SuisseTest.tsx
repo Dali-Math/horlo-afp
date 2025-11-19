72f4fd97ac576ae1b2f4ef5bed49ea780a3578cf
Dali-Math <momo18669@gmail.com>
2025-11-19 12:23:59 +0100

    Create SuisseTest component for testing purposes

---
+++ src/components/suisse/SuisseTest.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function SuisseTest() {
+  return (
+    <div style={{ padding: 40, color: "white" }}>
+      <h1>Page Suisse – Test</h1>
+      <p>Ceci est la version test.</p>
+    </div>
+  );
+}
