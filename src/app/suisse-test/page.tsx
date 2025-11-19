fc5f56298e1fb6a9586a9170a155f8431644cee2
Dali-Math <momo18669@gmail.com>
2025-11-19 12:27:05 +0100

    Refactor imports to use suisse components

--- src/app/suisse-test/page.tsx
+++ src/app/suisse-test/page.tsx
@@ -1,19 +1,19 @@
 'use client';
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './components/ThemeContext';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Features from './components/Features';
-import Collection from './components/Collection';
-import WatchDetail from './components/WatchDetail';
-import Footer from './components/Footer';
-import Movements from './components/Movements';
-import Glossary from './components/Glossary';
-import History from './components/History';
-import SwissMap from './components/SwissMap';
-import OfficialResources from './components/OfficialResources';
-import MasterQuotes from './components/MasterQuotes';
+import { ThemeProvider } from './components/suisse/ThemeContext';
+import Navbar from './components/suisse/Navbar';
+import Hero from './components/suisse/Hero';
+import Features from './components/suisse/Features';
+import Collection from './components/suisse/Collection';
+import WatchDetail from './components/suisse/WatchDetail';
+import Footer from './components/suisse/Footer';
+import Movements from './components/suisse/Movements';
+import Glossary from './components/suisse/Glossary';
+import History from './components/suisse/History';
+import SwissMap from './components/suisse/SwissMap';
+import OfficialResources from './components/suisse/OfficialResources';
+import MasterQuotes from './components/suisse/MasterQuotes';
 
 // Utility for optimizing static images in App
 const optimize = (url: string, width = 1200) => {
